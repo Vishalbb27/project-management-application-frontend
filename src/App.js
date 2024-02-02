a03971ae25d14abae306c1960015070ee8d4bdb5
Vishal <vishalbb178@gmail.com>
2024-02-02 11:34:25 +0530

    Request to client API for testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "http://pma-1-env.eba-v2xqi53u.us-west-2.elasticbeanstalk.com/api/clients"
+      )
+      .then((response) => {
+        setClients(response.data);
+      });
+  });
+
   return (
     <div className="App">
       <h1>Hello World it works update 3 successful</h1>
+      {clients.map((client) => (
+        <div>
+          <p>{client.c_name}</p>
+        </div>
+      ))}
     </div>
   );
 }
